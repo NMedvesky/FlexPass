0005fe684770e86fa8c97eff1ece332a4f99ba41
Quinn Melendez <quinnmelendez20@gmail.com>
2025-10-21 11:29:19 -0500

    Updated main function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
     print("Welcome to FlexPass!")
 
+def general_greeting():
+    print ("The FlexPass system is initializing")
+
+def shutdown_system():
+    print ("The FlexPass system is shutting down")
+    print ("Goodbye")
 
 if __name__ == "__main__":
     main()
+    general_greeting()
+    shutdown_system()
