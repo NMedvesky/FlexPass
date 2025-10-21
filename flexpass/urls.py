fcc1ebcfe6b6dd2dc27006c575870fb1cd650b32
Niko Medvesky <nikomedvesky@gmail.com>
2025-10-21 13:59:35 -0500

    Add Dashboard App

--- flexpass/urls.py
+++ flexpass/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("dashboard.urls")),
     path("classroom/", include("classroom.urls")),
     path("admin/", admin.site.urls),
 ]
