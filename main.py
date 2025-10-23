5452f7cc30d640bd3614f893be7ebb45fa63db62
Quinn Melendez <quinnmelendez20@gmail.com>
2025-10-23 09:03:07 -0500

    Added calendar feed to main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from ics import Calendar
-import requests
-url = "https://providencecatholic.myschoolapp.com/podium/feed/iCal.aspx?z=QkgRCgwN981Q0EgglEn%2bkPikCgRySC4Z2loStcQH36F6FTLrmYr0jPBDCuzbLyf%2bxkIdpJO0iNssXQzBZEhl4w%3d%3d"
+
 def main():
-    print("Welcome to FlexPass!")
-    cal = Calendar(requests.get(url).text)
-    print(cal.events)
+    with open("CalExport.ics", "r") as f:
+        file = f.read() 
+        c = Calendar(file)
+        print(c.events)
 
 if __name__ == "__main__":
     main()
