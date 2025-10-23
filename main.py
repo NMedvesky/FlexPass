14c8c49b9b5ccd9e5e213279ea3e3f9206aa22ef
Niko Medvesky <nikomedvesky@gmail.com>
2025-10-23 09:33:01 -0500

    Find current schedule

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
+from datetime import datetime
+
+import arrow
 from ics import Calendar
 
-def main():
+SCHEDULES = [
+    "7 Period Day",
+    "3 Period Block",
+    "4 Period Block",
+    "Mass Schedule",
+    "Assembly Schedule",
+]
+
+
+def get_today_schedule():
+    today = arrow.now().to("utc").floor("day")
+
     with open("CalExport.ics", "r") as f:
-        file = f.read() 
-        c = Calendar(file)
-        print(c.events)
+        file = f.read()
+
+    c = Calendar(file)
+    for event in c.events:
+        for schedule in SCHEDULES:
+            if schedule not in event.name:
+                continue
+
+            if event.begin.floor("day") == today:
+                return event.name
+
+    return "No School"
+
+
+def main():
+    print(get_today_schedule())
+
 
 if __name__ == "__main__":
     main()
